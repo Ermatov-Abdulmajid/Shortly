6f562b653aa460394df0daeebaf82c97a2a8deb0
abdumajidgh <ermatovabdumajid23@gmail.com>
2023-09-07 09:39:15 +0500

    add js news

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@ const elFeatureUrlResults = document.querySelector('.feature-url__results');
 
 foo.addEventListener('submit', function (evt) {
     evt.preventDefault(evt);
+    elFeatureUrlResults.classList.add('feature-url__results--open');
+});
 
-    elFeatureUrlResults.classList.add('feature-url__results--open')
+const elJsUrlShortener = document.querySelector('.js-url-shortener');
+
+elJsUrlShortener.addEventListener('click' , function (evt) {
+    if (evt.target.matches('.feature-url__button')) {
+        evt.target.textContent = 'Copied!';
+        evt.target.classList.add('feature-url__button--copied');
+        setTimeout( function ()  {
+            evt.target.textContent = 'Copy';
+            evt.target.classList.remove('feature-url__button--copied');
+        }, 1000);
+    };
 });
